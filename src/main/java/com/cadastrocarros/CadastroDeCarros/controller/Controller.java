b6591564d63bee87eb281bb759f602f8c2a40720
mickaelulivieri <mmickael.carvalho@gmail.com>
2025-03-27 16:03:38 -0300

    init: config inicial do projeto

---
+++ src/main/java/com/cadastrocarros/CadastroDeCarros/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.cadastrocarros.CadastroDeCarros.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas(){
+        return "Primeira rota";
+    }
+}
---
+++ src/test/java/com/cadastrocarros/CadastroDeCarros/CadastroDeCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadastrocarros.CadastroDeCarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
